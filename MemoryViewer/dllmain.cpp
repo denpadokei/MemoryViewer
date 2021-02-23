659752e10a9b938864f26f1abc07493db2967976
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-02-23 15:31:46 +0900

    不要include削除

--- MemoryViewer/dllmain.cpp
+++ MemoryViewer/dllmain.cpp
@@ -1,7 +1,6 @@
 ﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
 #include "pch.h"
 #include "dllmain.h"
-#include <exception>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
